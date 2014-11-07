4c592e344611595ab6346772e47e0dadeb4086ba
David Perry <curiousepic@gmail.com>
2014-11-07 14:42:09 -0500

    Fiddling with HTTParty

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
     if logged_in?
       twitter_auth = current_user.authorizations.find_by(provider: 'twitter')
       if twitter_auth
-        response = HTTParty.get("https://api.twitter.com/1.1/statuses/home_timeline.json",
-                                { authorization: "token #{twitter_auth.token}" })
+        response = HTTParty.get("https://api.twitter.com/1.1/statuses/home_timeline.json"
+                                 )
         @home = JSON.parse(response.body)
       end
     end
