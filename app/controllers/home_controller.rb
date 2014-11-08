e223705ea6986134393f922df2d0d0abcba6a7ab
David Perry <curiousepic@gmail.com>
2014-11-08 17:26:21 -0500

    Revised Tweeter list, sorting by tweet count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,11 +9,28 @@ class HomeController < ApplicationController
           config.access_token        = twitter_auth.token
           config.access_token_secret = twitter_auth.secret
         end
-        @user_list = []
-        @timeline = client.home_timeline(options = {count: 200})
-        @timeline.each do |t|
-          @user_list << { user_id: t.user, user_name: t.user.name , user_screen_name: t.user.screen_name }
+
+        timeline_users = []
+        timeline = client.home_timeline(options = {count: 200})
+        timeline.each do |t|
+          timeline_users << { user_id: t.user.id,
+                              user_name: t.user.name,
+                              user_screen_name: t.user.screen_name}
         end
+        user_list = []
+        timeline_users.each do |t|
+          user_index = user_list.find_index{ |h| h[:user_id] == t[:user_id] }
+          if user_index == nil
+            user_list << { user_id: t[:user_id],
+                           user_name: t[:user_name],
+                           user_screen_name: t[:user_screen_name],
+                           tweet_count: 1}
+          else
+            user_list[user_index][:tweet_count] += 1
+          end
+
+        end
+        @display_list = user_list.sort_by! { |u| u[:tweet_count]}.reverse
       end
     end
   end
