93777ec5316b4e37041be756ddbccebc4e92d91d
David Perry <curiousepic@gmail.com>
2014-11-09 11:53:29 -0500

    Set Client moved from index action to before_action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,49 @@
 class HomeController < ApplicationController
+  before_action :set_client
+
   def index
+    if logged_in?
+      timeline_users = []
+      timeline = @client.home_timeline(options = {count: 200})
+      timeline.each do |t|
+        timeline_users << { user_id: t.user.id,
+                            user_name: t.user.name,
+                            user_screen_name: t.user.screen_name}
+      end
+      user_list = []
+      timeline_users.each do |t|
+        user_index = user_list.find_index{ |h| h[:user_id] == t[:user_id] }
+        if user_index == nil
+          user_list << { user_id: t[:user_id],
+                         user_name: t[:user_name],
+                         user_screen_name: t[:user_screen_name],
+                         tweet_count: 1}
+        else
+          user_list[user_index][:tweet_count] += 1
+        end
+
+      end
+      @display_list = user_list.sort_by! { |u| u[:tweet_count]}.reverse
+    end
+  end
+
+
+  def mute_friend
+    @client.mute(params)
+  end
+
+  private
+
+  def set_client
     if logged_in?
       twitter_auth = current_user.authorizations.find_by(provider: 'twitter')
       if twitter_auth
-        client = Twitter::REST::Client.new do |config|
+        @client = Twitter::REST::Client.new do |config|
           config.consumer_key        = ENV['TWITTER_KEY']
           config.consumer_secret     = ENV['TWITTER_SECRET']
           config.access_token        = twitter_auth.token
           config.access_token_secret = twitter_auth.secret
         end
-        timeline_users = []
-        timeline = client.home_timeline(options = {count: 200})
-        timeline.each do |t|
-          timeline_users << { user_id: t.user.id,
-                              user_name: t.user.name,
-                              user_screen_name: t.user.screen_name}
-        end
-        user_list = []
-        timeline_users.each do |t|
-          user_index = user_list.find_index{ |h| h[:user_id] == t[:user_id] }
-          if user_index == nil
-            user_list << { user_id: t[:user_id],
-                           user_name: t[:user_name],
-                           user_screen_name: t[:user_screen_name],
-                           tweet_count: 1}
-          else
-            user_list[user_index][:tweet_count] += 1
-          end
-
-        end
-        @display_list = user_list.sort_by! { |u| u[:tweet_count]}.reverse
       end
     end
   end
